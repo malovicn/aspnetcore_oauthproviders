5e21b563c5a67cc035453e9c26a7088c562f2ffb
Nikola Malovic <nimal@microsoft.com>
2018-05-26 12:01:51 +0200

    Adding Asp.Net Core OAuth providers for LinkedIn and GitHub

---
+++ OAuthProviders/Linkedin/LinkedinOptions.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using Microsoft.AspNetCore.Http;
+
+namespace Nivatech.Framework.OAuthProviders.LinkedIn
+{
+    public class LinkedInOptions : OAuthOptions
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LinkedInOptions"/> class.
+        /// </summary>
+        public LinkedInOptions()
+        {
+            this.CallbackPath = new PathString("/signin-linkedin");
+            this.AuthorizationEndpoint = LinkedInDefaults.AuthorizationEndpoint;
+            this.TokenEndpoint = LinkedInDefaults.TokenEndpoint;
+
+            this.UserInformationEndpoint = LinkedInDefaults.UserInformationEndpoint;
+            this.Scope.Add("r_basicprofile");
+            this.Scope.Add("r_emailaddress");
+
+            this.ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "id", ClaimValueTypes.String);
+            this.ClaimActions.MapJsonKey(ClaimTypes.Name, "formattedName", ClaimValueTypes.String);
+            this.ClaimActions.MapJsonKey(ClaimTypes.Email, "emailAddress", ClaimValueTypes.Email);
+            this.ClaimActions.MapJsonKey("picture", "pictureUrl", ClaimValueTypes.String);
+        }
+    }
+}
---
+++ OAuthProviders/OAuthExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+using Nivatech.Framework.OAuthProviders.GitHub;
+using Nivatech.Framework.OAuthProviders.LinkedIn;
+
+namespace Nivatech.Framework.OAuthProviders
+{
+    public static class OAuthExtension
+    {
+        /// <summary>
+        /// Adds the LinkedIn oAuth provider
+        /// </summary>
+        /// <param name="builder">The builder.</param>
+        /// <param name="linkedinOptions">The GitHub options.</param>
+        /// <returns></returns>
+        public static AuthenticationBuilder AddGitHub(this AuthenticationBuilder builder, Action<GitHubOptions> githubOptions)
+        {
+            return builder.AddOAuth<GitHubOptions, GitHubHandler>("GitHub", githubOptions);
+        }
+
+        /// <summary>
+        /// Adds the LinkedIn oAuth provider
+        /// </summary>
+        /// <param name="builder">The builder.</param>
+        /// <param name="linkedinOptions">The LinkedIn options.</param>
+        /// <returns></returns>
+        public static AuthenticationBuilder AddLinkedin(this AuthenticationBuilder builder, Action<LinkedInOptions> linkedinOptions)
+        {
+            return builder.AddOAuth<LinkedInOptions, LinkedinHandler>("LinkedIn", linkedinOptions);
+        }
+    }
+}
